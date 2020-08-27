ce6890a8793ea27a59f1b3489611fb0faa6a92dc
Bishrut Bhattarai <bishrutbhattarai@gmail.com>
2020-08-27 16:06:24 -0500

    Added a welcome function

--- bot.py
+++ bot.py
@@ -1,19 +1,35 @@
 import discord
+from discord.ext import commands
 import os 
 from dotenv import load_dotenv
+import typing
 
 ## load Discord token from .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 ## Start bot client
-client = discord.Client()
+bot = commands.Bot(command_prefix="^")
 
 ## Test to see if bot can run
-@client.event
+@bot.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord")
-    print(f"The bot is currently in guilds: {client.guilds}")
+    print(f"{bot.user} has connected to Discord")
+    print(f"The bot is currently in guilds: {bot.guilds}")
+
+## Test to see if we can welcome a user in #general chat
+@bot.event
+async def on_member_join(member: discord.Member):
+    WELCOME_MESSAGE = f"Welcome to {member.guild.name}, {member.mention}"
+
+    welcome_channel = discord.utils.find(
+        lambda g: g.name=='general' and isinstance(g, discord.TextChannel), member.guild.channels
+    )
+    if welcome_channel:
+        await welcome_channel.send(content=WELCOME_MESSAGE)
+    else:
+        await member.create_dm()
+        await member.dm_channel.send(content=WELCOME_MESSAGE)
 
 ## Start bot
-client.run(TOKEN)
+bot.run(TOKEN)
